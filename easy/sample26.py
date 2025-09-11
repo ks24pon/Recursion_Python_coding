e422fa2df64a52e92d4bd41e92454c128f1dae29
ks24pon <pom123re0624@gmail.com>
2025-09-11 13:15:39 +0900

    https://recursionist.io/dashboard/course/1/lesson/79

--- easy/sample26.py
+++ easy/sample26.py
@@ -3,7 +3,7 @@ def insertUnderscoreAt(s,i):
     front = s[:i]
     # アンダースコア後の文字列
     back = s[i:]
-    if i > len(s):
+    if i >= len(s):
         return s # iが受け取った文字列サイズ以上の場合
     else:
         return front + "_" + back # それ以外の場合「_」を入れた文字列を返す
