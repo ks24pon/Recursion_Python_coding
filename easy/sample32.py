d50d9f2921fb1eaea60d2b9f47b5b8633939ecd2
ks24pon <pom123re0624@gmail.com>
2025-09-12 13:52:05 +0900

    https://recursionist.io/dashboard/problems/353

---
+++ easy/sample32.py
@@ -0,0 +1,21 @@
+import math
+
+def calculateCorporationTax(state,year,profit):
+    if isLeapYear(year):
+        return math.ceil(profit * stateTax(state))
+    else:
+        return math.ceil(profit * stateTax(state) + profit * 0.21)
+
+# 州名を受け取り、州税を計算する関数
+def stateTax(state):
+    if state == "AZ": return 0.049
+    elif state == "CA": return 0
+    elif state == "NC": return 0.025
+    else: return 0.05
+
+# 閏年チェック関数
+def isLeapYear(year):
+    if year % 400 == 0: return True
+    elif year % 100 == 0: return False
+    elif year % 4 == 0: return True
+    else: return False
