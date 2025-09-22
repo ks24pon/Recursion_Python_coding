feceb4d0a81b647e740b580ccd614caafa9434da
ks24pon <pom123re0624@gmail.com>
2025-09-22 10:59:10 +0900

    https://recursionist.io/dashboard/problems/498

---
+++ easy/sample48.py
@@ -0,0 +1,53 @@
+class Animal:
+    def __init__(self, species, weightKg, heightM, predator):
+        self.species = species
+        self.weightKg = weightKg
+        self.heightM = heightM
+        self.predator = predator
+
+    def domesticate(self):
+        self.predator = False
+
+class Hunter:
+    def __init__(self, name, weightKg, heightM, strength, cageCubicMeters):
+        self.name = name
+        self.weightKg = weightKg
+        self.heightM = heightM
+        self.strength = strength
+        self.cageCubicMeters = cageCubicMeters
+
+    def strengthKg(self):
+        return self.strength * self.weightKg
+
+    def canCaptureAnimal(self, animal):
+        if self.strengthKg() >= animal.weightKg and self.cageCubicMeters >= animal.heightM and not animal.predator:
+            return True
+        return False
+
+    def attemptToDomesticate(self, animal):
+        if self.strengthKg() > animal.weightKg * 2:
+            animal.domesticate()
+            return True
+        return False
+
+def capturedAnimals(hunter, animals):
+    for animal in animals:
+        if hunter.canCaptureAnimal(animal):
+            print(animal.species)
+    
+
+def domesticateTheAnimals(hunter, animals):
+    for animal in animals:
+        animal.domesticate()
+
+tiger = Animal("Tiger", 290, 2.6, True)
+cow = Animal("Cow", 1134, 1.5, False)
+snake = Animal("Snake", 100, 1.2, True)
+cat = Animal("Cat", 10, 0.5, False)
+hunternator = Hunter("Hunternator", 124.73, 1.85, 15, 3)
+
+animals = [tiger, cow, snake, cat]
+capturedAnimals(hunternator, animals)
+
+domesticateTheAnimals(hunternator, animals)
+capturedAnimals(hunternator, animals)
