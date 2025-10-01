1b8969a329343fc71c16ab31c10d4be2330ab7be
ks24pon <pom123re0624@gmail.com>
2025-10-01 12:00:03 +0900

    修正

--- easy/sample50.py
+++ easy/sample50.py
@@ -27,7 +27,7 @@ def printHonorsStudents(school):
         for student in classroom.students:
             if student.gradeLevel>=10:
                 print(student.getStudentInfo()+" from "+classroom.teacher+"'s class")
-            
+
 classroom1 = Classroom([Student("AC-343424", "James", "Smith", 6), Student("AC-343428", "Maria", "Garcia", 5), Student("AC-343434", "Robert", "Johnson", 3), Student("AC-343454","Danny", "Robertson",10)],  "Algebra II", "Emily Theodore")
 
 classroom2 = Classroom([Student("AC-340014","Kent", "Carter",9), Student("AC-340024","Isaiah", "Chambers",10), Student("AC-340018","Leta", "Ferguson", 7)],  "English", "Daniel Pherb")
