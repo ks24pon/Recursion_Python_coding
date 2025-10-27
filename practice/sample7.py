2200c43a49881adaf579cc6c2b825e57f1fa4ee0
ks24pon <pom123re0624@gmail.com>
2025-10-27 15:28:11 +0900

    substring

---
+++ practice/sample7.py
@@ -0,0 +1,10 @@
+# 例題1
+# recursion@info.comの@より後ろを切り取りましょう
+email = "recursion@info.com";
+# printで出力してください
+print(email[9:])
+
+# 例題 2
+# Steve Jobs の Jobs を切り取りましょう
+# print で出力してください
+print("Steve Jobs"[-4:])
