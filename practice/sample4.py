20dcf76f2e08ca855347775b87c1b526beb952de
ks24pon <pom123re0624@gmail.com>
2025-10-27 11:15:29 +0900

    論理演算子

---
+++ practice/sample4.py
@@ -0,0 +1,7 @@
+def isVowel(word):
+    return word == "a" or word == "i" or word == "u" or word == "e" or word == "0"
+
+print(isVowel("a"))
+print(isVowel("e"))
+print(isVowel("z"))
+print(isVowel("k"))
