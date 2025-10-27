f6fbe7e2cc1ba3a3c2bf811fdfad36a7d722de9b
ks24pon <pom123re0624@gmail.com>
2025-10-27 15:08:42 +0900

    大文字.小文字

---
+++ practice/sample6.py
@@ -0,0 +1,8 @@
+# 変数の中にある文字列を全て大文字、小文字で表示してみましょう
+cityName = "California"
+
+# uppercase 関数を使って、print で出力してみましょう (CALIFORNIA)
+print(cityName.upper())
+
+# lowercase 関数を使って、print で出力してみましょう (california)
+print(cityName.lower())
