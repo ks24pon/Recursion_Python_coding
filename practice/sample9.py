b99b1577d42d22fa795773bc88d8b4d78541fbb3
ks24pon <pom123re0624@gmail.com>
2025-10-28 09:23:39 +0900

    replace

---
+++ practice/sample9.py
@@ -0,0 +1,10 @@
+# 例題1
+# 文字列 Hello の全ての l を p に置換してください
+# print で出力してください
+print("Hello".replace("l","p"))
+
+# 例題 2
+# 変数 sentence の中にある文字列の I を、変数 lastName を使って置換してください (I --> Albert)
+# print で出力してください (Albert will go hiking near a ranch in Oregon.)
+print(sentence.replace("I",lastName))
+
