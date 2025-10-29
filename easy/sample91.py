b71677dbcd86125d20c10a26bb11824a33cd6f26
ks24pon <pom123re0624@gmail.com>
2025-10-29 12:57:19 +0900

    https://recursionist.io/dashboard/problems/350

---
+++ easy/sample91.py
@@ -0,0 +1,13 @@
+import math
+
+def calculateGoalMoney(capital,year):
+
+    def calculateInterestRate(capital):
+        if capital % 2 == 0:
+            return 0.02
+        else:
+            return 0.03
+
+    rate = calculateInterestRate(capital)
+    amount = capital * math.pow(1+rate,year)
+    return math.floor(amount)
