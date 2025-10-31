9812e2b5369ef516dd8b86870f1d8d729664efe2
ks24pon <pom123re0624@gmail.com>
2025-10-31 14:46:34 +0900

    修正

--- easy/sample108.py
+++ easy/sample108.py
@@ -1,6 +1,4 @@
 def countDivisibleByK(n,k):
     # ベースケース
-    if n % k != 0:
-        return 0
-    else:
-        return 1 + countDivisibleByK(n//k,k)
+    if n % k != 0: return 0
+    else: return 1 + countDivisibleByK(n//k,k)
