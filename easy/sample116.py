abfbc435408119613f3e941def3e2da384a036e5
ks24pon <pom123re0624@gmail.com>
2025-11-06 17:22:12 +0900

    オブジェクト

---
+++ easy/sample116.py
@@ -0,0 +1,14 @@
+class Rectangle:
+  def __init__(self, w, h, c):
+    self.width = w
+    self.height = h
+    self.color = c
+
+square1 = Rectangle(20,20,"orange")
+print(square1.color)
+
+rectangle1 = Rectangle(46,30,"blue")
+print(rectangle1.width)
+
+rectangle2 = Rectangle(20,100,"purple")
+print(rectangle1.height)
