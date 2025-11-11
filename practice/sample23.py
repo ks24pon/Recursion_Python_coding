20f81127f90a088ab60df07e670579351ab6fbfc
ks24pon <pom123re0624@gmail.com>
2025-11-11 13:29:42 +0900

    dynamic_array

---
+++ practice/sample23.py
@@ -0,0 +1,18 @@
+def printArray(arr):
+  for value in arr:
+    print(value, end = " ")
+  print()
+
+dArr = [2,3,4,1-10,200]
+
+printArray(dArr)
+
+# 追加
+dArr.append(10)
+dArr.append(340)
+dArr.append(543)
+dArr.append(-21)
+
+# 出力
+printArray(dArr)
+
