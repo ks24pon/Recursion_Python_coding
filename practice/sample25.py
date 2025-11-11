0b356ce06d4e97e097425bb114eaf548617d91c9
ks24pon <pom123re0624@gmail.com>
2025-11-11 15:35:52 +0900

    array

--- practice/sample25.py
+++ practice/sample25.py
@@ -1,11 +1,14 @@
 def cubeRange(a, b):
   cubeList = []
+  # 3~10までループ
   for i in range(a, b + 1):
+    # iの3乗をリストに追加
     cubeList.append(i ** 3)
   return cubeList
 
 def printList(arr):
+  # 配列の長さを取得
   for i in range(len(arr)):
+    # iの回数分だけループ
     print(arr[i])
-
 printList(cubeRange(3,10))
