b8794f75203bd1b5c23de4e0eb1a4be253d5c6e0
ks24pon <pom123re0624@gmail.com>
2025-11-12 15:17:10 +0900

    fix

--- practice/sample29.py
+++ practice/sample29.py
@@ -9,11 +9,13 @@ class SinglyLinkedList:
     def __init__(self, node):
         # リストの先頭
         self.head = node
-arr = [35,23,546,67,86,234,56,767,34,1,98,78,555]
+
+
+arr = [35, 23, 546, 67, 86, 234, 56, 767, 34, 1, 98, 78, 555]
 numList = SinglyLinkedList(Node(arr[0]))
 # ノード作成後、リストの先頭35
-currentNode = numList.head;
-for i in range(1,len(arr)):
+currentNode = numList.head
+for i in range(1, len(arr)):
     # 次のノードに繋ぐ
     currentNode.next = Node(arr[i])
     # .nextで次のノード
