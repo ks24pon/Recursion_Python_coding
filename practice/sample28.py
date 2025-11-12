cb48cf091e9ddb0dcdf61b2b2371b1996f62d10c
ks24pon <pom123re0624@gmail.com>
2025-11-12 10:36:08 +0900

    2array

---
+++ practice/sample28.py
@@ -0,0 +1,8 @@
+def print2dArray(array2d):
+    for string in array2d:
+        for char in string:
+            print(char)
+
+
+stringArray = ["Hello World", "Fizz", "Buzz", "Foo", "Bar"]
+print2dArray(stringArray)
