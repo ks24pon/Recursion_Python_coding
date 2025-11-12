a39fa6f8afad64e92066419084522617a4c98f6a
ks24pon <pom123re0624@gmail.com>
2025-11-12 10:26:08 +0900

    2array

---
+++ practice/sample27.py
@@ -0,0 +1,9 @@
+def print2dArray(array2d):
+    for arr in array2d:
+        print(arr)
+        for value in arr:
+            print(value)
+
+
+array2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print2dArray(array2d)
