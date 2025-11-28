6aedd37fe13379dab1dba4920bcc17c97dab1dc4
ks24pon <pom123re0624@gmail.com>
2025-11-28 10:05:14 +0900

    lengthプロパティ

--- veryeasy/sample93.py
+++ veryeasy/sample93.py
@@ -1,3 +1,4 @@
-print("Recursion"[0])
-print("Recursion"[4])
-print("Recursion"[-1])
+print(len("Computer"))
+print(len("recursion@info.com"))
+print("Computer"[len("Computer")-1])
+print("recursion@info.com"[len("recursion@info.com")-1])
