7aca207c1838cfe4d0f7cbaedc7ed86e09cbca80
ks24pon <pom123re0624@gmail.com>
2025-11-29 18:51:39 +0900

    変数と代入演算子 very easy

---
+++ veryeasy/sample97.py
@@ -0,0 +1,14 @@
+myPower = 300
+mySpeed = 200
+myTechnique = 50
+myDefense = 500
+
+myPower += 10
+mySpeed -= 5
+myTechnique *= 7
+myDefense //= 2
+
+print("myPower : " + str(myPower))
+print("mySpeed : " + str(mySpeed))
+print("myTechnique : " + str(myTechnique))
+print("myDefense : " + str(myDefense))
