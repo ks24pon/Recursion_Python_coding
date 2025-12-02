9aa95d8a0d199162a5e63a0b61894ebda30e9114
ks24pon <pom123re0624@gmail.com>
2025-12-02 10:35:32 +0900

    演算子

---
+++ practice/sample46.py
@@ -0,0 +1,10 @@
+def isVowel(word):
+  if word == "a" or word == "i" or word == "u" or word == "e" or word == "o":
+    return True
+  else:
+    return False
+  
+print(isVowel("a"))
+print(isVowel("e"))
+print(isVowel("z"))
+print(isVowel("k"))
