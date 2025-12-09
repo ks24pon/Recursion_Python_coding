e65b70d2b4ba106b97ba4d0033934b4c25cfae19
ks24pon <pom123re0624@gmail.com>
2025-12-09 10:27:52 +0900

    共通の接頭辞

---
+++ easy/sample175.py
@@ -0,0 +1,9 @@
+def commonPrefix(s1, s2):
+    # ベースケース
+    if s1 == "" or s2 == "":
+        return ""
+    
+    if s1[0] != s2[0]:
+        return ""
+    
+    return s1[0] + commonPrefix(s1[1:],s2[1:])
