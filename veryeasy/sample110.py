eb90c64321d4af1a007ca0c38bdf8443941e0b2d
ks24pon <pom123re0624@gmail.com>
2025-12-10 15:09:22 +0900

    ハミング距離（文字列）

---
+++ veryeasy/sample110.py
@@ -0,0 +1,9 @@
+def hammingDistanceInString(string1,string2):
+    # 初期値
+    count = 0
+
+    for i in range(len(string1)):
+        if string1[i] != string2[i]:
+            count += 1
+    
+    return count
