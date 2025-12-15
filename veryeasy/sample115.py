68e5c524c5d7959403174fd900ecc8818abbf39a
ks24pon <pom123re0624@gmail.com>
2025-12-15 13:47:19 +0900

    最大値のインデックス

--- veryeasy/sample115.py
+++ veryeasy/sample115.py
@@ -4,6 +4,7 @@ def getMaxIndex(arr):
 
     # 1つずつ比較して大きければ更新
     for i in range(len(arr)):
-        if arr[i] > arr[maxIndex]:
+        if arr[i] >= arr[maxIndex]:
             maxIndex = i
     return maxIndex
+
