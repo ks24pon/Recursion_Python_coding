9d014123371b8ec9839dd4331921e6d71f8406f3
ks24pon <pom123re0624@gmail.com>
2025-12-18 16:47:01 +0900

    アルファベット判定

---
+++ easy/sample210.py
@@ -0,0 +1,6 @@
+def isVowel(alphabet):
+    alphabets= alphabet.lower()
+    if alphabets == "a" or alphabets == "i" or alphabets == "u" or alphabets == "e" or alphabets == "o":
+        return True
+    return False
+
